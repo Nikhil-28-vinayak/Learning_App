9467d97966bae3446335d3a4e18c869feb49659d
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2026-01-16 22:22:59 +0530

    Splash screen is completed

---
+++ app/src/main/java/com/example/learningapp/screens/loginscreen/LoginScreen.kt
@@ -0,0 +1,2 @@
+package com.example.learningapp.screens.loginscreen
+
---
+++ app/src/main/java/com/example/learningapp/screens/splashscreen/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.learningapp.screens.splashscreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.learningapp.R
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SplashScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+       Image(painter = painterResource(R.drawable.img),
+           contentDescription = "Logo",
+           modifier = Modifier.size(350.dp)
+       )
+    }
+}
